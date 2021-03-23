86b7fe8569ee9ccd91cbd43a1d6e5f670264de8b
geetasri p <geeta.peddireddi@gmail.com>
2021-03-23 11:11:42 -0700

    1.3: JavaScript Basics Part 2 - Added Pokemon loop and Printing data on the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ let pokemonList = [
   { name: "Genesect", height: 0.6, type: ["steel", "bug"] },
   { name: "HappBulbasauriny", height: 0.7, type: ["grass", "poison"] }
 ];
+let pokemonSizeThreshold = 0.7;
+
+for(let i = 0; i <= pokemonList.length; i++){
+    let pokemonString = `${pokemonList[i].name} ( Height: ${ pokemonList[i].height} )`;
+    let pokeman = pokemonList[i].height >= pokemonSizeThreshold ? `<p> ${pokemonString} - Wow, that’s big! </p>` : `<p> ${pokemonString} </p>`;
+    document.write(pokeman);
+}
