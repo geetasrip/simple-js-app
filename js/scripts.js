6537d9672740187541941a79a369f3e63979b4af
geetasri p <geeta.peddireddi@gmail.com>
2021-04-02 09:51:35 -0700

    1.5: JavaScript Functions Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-let pokemonList = [
-  { name: "Happiny", height: 0.6, type: ["normal"] },
-  { name: "Genesect", height: 0.6, type: ["steel", "bug"] },
-  { name: "HappBulbasauriny", height: 0.7, type: ["grass", "poison"] }
-];
-let pokemonSizeThreshold = 0.7;
+let pokemonRepository = (function() {
+  let pokemonList = [
+    { name: "Happiny", height: 0.6, type: ["normal"] },
+    { name: "Genesect", height: 0.6, type: ["steel", "bug"] },
+    { name: "HappBulbasauriny", height: 0.7, type: ["grass", "poison"] }
+  ]; 
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
 
-for(let i = 0; i <= pokemonList.length; i++){
-    let pokemonString = `${pokemonList[i].name} ( Height: ${ pokemonList[i].height} )`;
-    let pokeman = pokemonList[i].height >= pokemonSizeThreshold ? `<p> ${pokemonString} - Wow, that’s big! </p>` : `<p> ${pokemonString} </p>`;
-    document.write(pokeman);
-}
+let pokemonSizeThreshold = 0.7;
+pokemonRepository.getAll().forEach(function(pokemon) {
+  let pokemonString = `${pokemon.name} ( Height: ${pokemon.height} )`;
+  let pokemanItem =
+    pokemon.height >= pokemonSizeThreshold
+      ? `<p> ${pokemonString} - Wow, that’s big! </p>`
+      : `<p> ${pokemonString} </p>`;
+  document.write(pokemanItem);
+});
