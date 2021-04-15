dad5eaee84ecb4ed0c61b01a84c2ef0445a9b4ed
geetasri p <geeta.peddireddi@gmail.com>
2021-04-14 17:56:12 -0700

    1.8: Complex UI Elements with JS

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,58 @@ let pokemonRepository = (function() {
     });
   }
   function showDetails(pokemon) {
+      console.log("pokemon",pokemon);
+    let modalContainer = document.querySelector("#modal-container");
+
+    // Clear all existing modal content
+    modalContainer.innerHTML = "";
+
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    // Add the new modal content
+    let closeButtonElement = document.createElement("button");
+    closeButtonElement.classList.add("modal-close");
+    closeButtonElement.innerText = "Close";
+
+    let titleElement = document.createElement("h1");
+    titleElement.innerText = pokemon.name;
+
+    let contentElement = document.createElement("p");
+    contentElement.innerText = 'Model text';
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add("is-visible");
+
+    
+closeButtonElement.addEventListener('click', hideModal);
+
+window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+  
+
     pokemonRepository.loadDetails(pokemon);
+  }
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
   }
   function LoadList() {
     return fetch("https://pokeapi.co/api/v2/pokemon/")
