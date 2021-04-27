791ab565dd5fbaea92fa453ca869f36e2b37e5d5
geetasri p <geeta.peddireddi@gmail.com>
2021-04-27 12:44:16 -0700

    Create practice.js

---
+++ js/practice.js
@@ -0,0 +1,43 @@
+// var num1, num2;
+// num1 = window.prompt("Input the First integer", "0");
+// num2 = window.prompt("Input the second integer", "0");
+                                                 
+// if(parseInt(num1, 10) > parseInt(num2, 10)) 
+//   { 
+//   console.log("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");
+//   }   
+// else
+//   if(parseInt(num2, 10) > parseInt(num1, 10)) 
+//   {
+//   console.log("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");
+//   }                  
+// else
+//   {
+//    console.log("The values "+ num1+ " and "+num2+ " are equal.");
+//   }
+
+
+//   function findNumberSign(num1, num2, num3){
+    
+//          if(Math.sign(num1) == "-1" ){
+//              console.log("Math.sign(num1)",Math.sign(num1))
+//             var digits = num1.toString().split(''); 
+//         alert("sign is " + digits[0]) 
+//     }
+//     if(Math.sign(num2) == "-1" ){
+//         console.log("Math.sign(num2)",Math.sign(num2))
+//         var digits2 = num2.toString().split(''); 
+//         console.log("digits2",digits2[0]);
+//     alert("sign is " + digits2[0]) 
+// }
+// if(Math.sign(num3) == "-1" ){
+//     console.log("Math.sign(num3)",Math.sign(num1))
+//     var digits3 = num3.toString().split(''); 
+// alert("sign is " ,digits3[0]) 
+// }
+//   }
+
+
+function sortNumber(num1, num2, num3){
+
+}
